3b5609170b3b9726311bfaf0903334dec1817d8f
temu-05 <99035848+temu-05@users.noreply.github.com>
2022-04-16 22:42:53 +0300

    Create get_match_func.c

---
+++ get_match_func.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * get_match_func - match format with function format
+ * @format: It's a character string
+ * @list: the list of arguments the function _printf is receiving
+ * Return: the number of character the function is printing
+ */
+int get_match_func(const char *format, va_list list, st_fmt st_format[])
+{
+	int count = 0, i = 0, j, k = 0, num = 0;
+
+	for (i = 0; format && format[i] != 0; i++)
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+			count += 1;
+		}
+		else
+		{
+			for (j = 0; st_format[j].fmt; j++)
+			{
+				if (format[i + 1] == st_format[j].fmt[k])
+				{
+					num = st_format[j].func(list);
+					count += num;
+					i++;
+					break;
+				}
+			}
+			if (st_format[j].fmt == NULL && format[i + 1] != ' ')
+			{
+				if (format[i + 1] != 0)
+				{
+					_putchar(format[i]);
+					_putchar(format[i + 1]);
+					count += 2;
+					i++;
+				}
+				else
+					return (-1);
+			}
+		}
+	}
+	if (format == NULL)
+		return (-1);
+	return (count);
+
+
+}
